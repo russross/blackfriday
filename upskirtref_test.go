00330f85be6d01645d05974e8f4e94c473f3687e
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-06-01 18:12:57 -0700

    fixes to the test runner

--- upskirtref_test.go
+++ upskirtref_test.go
@@ -17,42 +17,12 @@ import (
 	"testing"
 )
 
-func runMarkdown(input string) string {
-	renderer := HtmlRenderer(0)
-	return string(Markdown([]byte(input), renderer, 0))
-}
-
 // disregard dos vs. unix line endings differences
 func normalizeEol(s string) string {
 	return strings.Replace(s, "\r\n", "\n", -1)
 }
 
-func doTests(t *testing.T, files []string) {
-	for _, basename := range files {
-		fn := filepath.Join("upskirtref", basename+".text")
-		actualdata, err := ioutil.ReadFile(fn)
-		if err != nil {
-			t.Errorf("Couldn't open '%s', error: %v\n", fn, err)
-			continue
-		}
-		fn = filepath.Join("upskirtref", basename+"_upskirt_ref.html")
-		expecteddata, err := ioutil.ReadFile(fn)
-		if err != nil {
-			t.Errorf("Couldn't open '%s', error: %v\n", fn, err)
-			continue
-		}
-
-		actual := string(actualdata)
-		actual = normalizeEol(string(runMarkdown(actual)))
-		expected := normalizeEol(string(expecteddata))
-		if actual != expected {
-			t.Errorf("\nFile   [%#v]\nExpected[%#v]\nActual  [%#v]",
-				basename+".text", expected, actual)
-		}
-	}
-}
-
-func TestCodeSpan(t *testing.T) {
+func TestMardownFiles(t *testing.T) {
 	files := []string{
 		"Amps and angle encoding",
 		"Auto links",
@@ -77,5 +47,28 @@ func TestCodeSpan(t *testing.T) {
 		"Tabs",
 		"Tidyness",
 	}
-	doTests(t, files)
+
+	for _, basename := range files {
+		fn := filepath.Join("upskirtref", basename+".text")
+		actualdata, err := ioutil.ReadFile(fn)
+		if err != nil {
+			t.Errorf("Couldn't open '%s', error: %v\n", fn, err)
+			continue
+		}
+		fn = filepath.Join("upskirtref", basename+"_upskirt_ref.html")
+		expecteddata, err := ioutil.ReadFile(fn)
+		if err != nil {
+			t.Errorf("Couldn't open '%s', error: %v\n", fn, err)
+			continue
+		}
+
+		actual := string(actualdata)
+		renderer := HtmlRenderer(0)
+		actual = normalizeEol(string(Markdown([]byte(actual), renderer, 0)))
+		expected := normalizeEol(string(expecteddata))
+		if actual != expected {
+			t.Errorf("\nFile    [%#v]\nExpected[%#v]\nActual  [%#v]",
+				basename+".text", expected, actual)
+		}
+	}
 }
