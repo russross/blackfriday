b94711cd858b7085e985b3452912ec4e97a55e16
Miek Gieben <miek@miek.nl>
2014-12-02 11:20:43 +0000

    Folds test back into the file again

--- inline_test.go
+++ inline_test.go
@@ -797,3 +797,63 @@ func TestFootnotesWithParameters(t *testing.T) {
 
 	doTestsInlineParam(t, tests, EXTENSION_FOOTNOTES, HTML_FOOTNOTE_RETURN_LINKS, params)
 }
+
+
+func runMarkdownInlineXML(input string, extensions, xmlFlags int) string {
+	extensions |= EXTENSION_AUTOLINK
+	extensions |= EXTENSION_STRIKETHROUGH
+	extensions |= EXTENSION_INDEX
+	extensions |= EXTENSION_CITATION
+
+	renderer := XmlRenderer(xmlFlags)
+
+	return string(Markdown([]byte(input), renderer, extensions))
+}
+
+func doTestsInlineXML(t *testing.T, tests []string) {
+	doTestsInlineParamXML(t, tests, 0, 0)
+}
+
+func doTestsInlineParamXML(t *testing.T, tests []string, extensions, xmlFlags int) {
+	var candidate string
+
+	for i := 0; i+1 < len(tests); i += 2 {
+		input := tests[i]
+		candidate = input
+		expected := tests[i+1]
+		actual := runMarkdownInlineXML(candidate, extensions, xmlFlags)
+		if actual != expected {
+			t.Errorf("\nInput   [%#v]\nExpected[%#v]\nActual  [%#v]",
+				candidate, expected, actual)
+		}
+
+		// now test every substring to stress test bounds checking
+		if !testing.Short() {
+			for start := 0; start < len(input); start++ {
+				for end := start + 1; end <= len(input); end++ {
+					candidate = input[start:end]
+					_ = runMarkdownInlineXML(candidate, extensions, xmlFlags)
+				}
+			}
+		}
+	}
+}
+
+func TestIndexXML(t *testing.T) {
+	var tests = []string{
+		"(((Tiger, Cats)))\n",
+		"<t>\n<iref item=\"Tiger\" subitem=\"Cats\"/>\n</t>\n",
+
+		"(((Tiger, Cats))\n",
+		"<t>\n(((Tiger, Cats))\n</t>\n",
+	}
+	doTestsInlineXML(t, tests)
+}
+
+func TestCitationXML(t *testing.T) {
+	var tests = []string{
+		"(((Tiger, Cats)))\n",
+		"<t>\n<iref item=\"Tiger\" subitem=\"Cats\"/>\n</t>\n",
+	}
+	doTestsInlineXML(t, tests)
+}
