bf18be9679959b62e78bd59505355e98fd94a03b
Miek Gieben <miek@miek.nl>
2014-12-23 11:58:31 +0000

    Fmt

--- html.go
+++ html.go
@@ -12,18 +12,18 @@ import (
 
 // Html renderer configuration options.
 const (
-	HTML_SKIP_HTML                = 1 << iota // skip preformatted HTML blocks
-	HTML_SKIP_STYLE                           // skip embedded <style> elements
-	HTML_SKIP_IMAGES                          // skip embedded images
-	HTML_SKIP_LINKS                           // skip all links
-	HTML_SAFELINK                             // only link to trusted protocols
-	HTML_NOFOLLOW_LINKS                       // only link with rel="nofollow"
-	HTML_HREF_TARGET_BLANK                    // add a blank target
-	HTML_TOC                                  // generate a table of contents
-	HTML_OMIT_CONTENTS                        // skip the main contents (for a standalone table of contents)
-	HTML_COMPLETE_PAGE                        // generate a complete HTML page
-	HTML_USE_XHTML                            // generate XHTML output instead of HTML
-	HTML_FOOTNOTE_RETURN_LINKS                // generate a link at the end of a footnote to return to the source
+	HTML_SKIP_HTML             = 1 << iota // skip preformatted HTML blocks
+	HTML_SKIP_STYLE                        // skip embedded <style> elements
+	HTML_SKIP_IMAGES                       // skip embedded images
+	HTML_SKIP_LINKS                        // skip all links
+	HTML_SAFELINK                          // only link to trusted protocols
+	HTML_NOFOLLOW_LINKS                    // only link with rel="nofollow"
+	HTML_HREF_TARGET_BLANK                 // add a blank target
+	HTML_TOC                               // generate a table of contents
+	HTML_OMIT_CONTENTS                     // skip the main contents (for a standalone table of contents)
+	HTML_COMPLETE_PAGE                     // generate a complete HTML page
+	HTML_USE_XHTML                         // generate XHTML output instead of HTML
+	HTML_FOOTNOTE_RETURN_LINKS             // generate a link at the end of a footnote to return to the source
 )
 
 var (
