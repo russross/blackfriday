20f86dd89f1f3730b57c39c03342bde993212df0
Miek Gieben <miek@miek.nl>
2015-01-19 21:44:59 +0000

    This was the v2 one

--- v3-rng.sh
+++ v3-rng.sh
@@ -3,5 +3,7 @@
 # ./mmark/mmark -xml -page mmark2rfc.md > x.xml
 # xmllint --relaxng xml2rfcv3.rng x.xml 
 
-ruby -r rexml/document -r open-uri -e "puts REXML::XPath.first(REXML::Document.new(open(%q{http://tools.ietf.org/id/draft-iab-xml2rfcv2-00.xml})),%{//artwork[@type='application/relax-ng-compact-syntax']}).text" > xml2rfcv3.rnc && \
-    trang xml2rfcv3.rnc xml2rfcv3.rng && rm xml2rfcv3.rnc
+THIS IS THE V2 ONE
+
+#ruby -r rexml/document -r open-uri -e "puts REXML::XPath.first(REXML::Document.new(open(%q{http://tools.ietf.org/id/draft-iab-xml2rfcv2-00.xml})),%{//artwork[@type='application/relax-ng-compact-syntax']}).text" > xml2rfcv3.rnc && \
+#    trang xml2rfcv3.rnc xml2rfcv3.rng && rm xml2rfcv3.rnc
