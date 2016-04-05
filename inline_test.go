russross/blackfriday

commit e02c392dc656371e48ec50c6b0acdfbfbee31439

Extract useful code to separate func

html.go
@@ -722,6 +722,29 @@ func isHtmlTag(tag []byte, tagname string) bool {
 	return found
 }
 
+// Look for a character, but ignore it when it's in any kind of quotes, it
+// might be JavaScript
+func skipUntilCharIgnoreQuotes(html []byte, start int, char byte) int {
+	inSingleQuote := false
+	inDoubleQuote := false
+	inGraveQuote := false
+	i := start
+	for i < len(html) {
+		switch {
+		case html[i] == char && !inSingleQuote && !inDoubleQuote && !inGraveQuote:
+			return i
+		case html[i] == '\'':
+			inSingleQuote = !inSingleQuote
+		case html[i] == '"':
+			inDoubleQuote = !inDoubleQuote
+		case html[i] == '`':
+			inGraveQuote = !inGraveQuote
+		}
+		i++
+	}
+	return start
+}
+
 func findHtmlTagPos(tag []byte, tagname string) (bool, int) {
 	i := 0
 	if i < len(tag) && tag[0] != '<' {
@@ -750,23 +773,9 @@ func findHtmlTagPos(tag []byte, tagname string) (bool, int) {
 		return false, -1
 	}
 
-	// Now look for closing '>', but ignore it when it's in any kind of quotes,
-	// it might be JavaScript
-	inSingleQuote := false
-	inDoubleQuote := false
-	inGraveQuote := false
-	for i < len(tag) {
-		switch {
-		case tag[i] == '>' && !inSingleQuote && !inDoubleQuote && !inGraveQuote:
-			return true, i
-		case tag[i] == '\'':
-			inSingleQuote = !inSingleQuote
-		case tag[i] == '"':
-			inDoubleQuote = !inDoubleQuote
-		case tag[i] == '`':
-			inGraveQuote = !inGraveQuote
-		}
-		i++
+	rightAngle := skipUntilCharIgnoreQuotes(tag, i, '>')
+	if rightAngle > i {
+		return true, rightAngle
 	}
 
 	return false, -1

commit 7e9a57463ff1634bca3967415fb5c6e841eb8c3a

Implement SkipStyle, add test
Fix a bug in findHtmlTagPos introduced with e02c392d.

html.go
@@ -884,7 +884,7 @@ func findHtmlTagPos(tag []byte, tagname string) (bool, int) {
 	}
 
 	rightAngle := skipUntilCharIgnoreQuotes(tag, i, '>')
-	if rightAngle > i {
+	if rightAngle >= i {
 		return true, rightAngle
 	}
 
@@ -1133,6 +1133,9 @@ func (r *HTML) RenderNode(w io.Writer, node *Node, entering bool) WalkStatus {
 			r.out(w, tag("/del", nil, false))
 		}
 	case HTMLSpan:
+		if r.flags&SkipStyle != 0 && isHtmlTag(node.Literal, "style") {
+			break
+		}
 		//if options.safe {
 		//	out(w, "<!-- raw HTML omitted -->")
 		//} else {

inline_test.go
@@ -1139,6 +1139,15 @@ func TestSkipImages(t *testing.T) {
 	})
 }
 
+func TestSkipStyle(t *testing.T) {
+	doTestsInlineParam(t, []string{
+		"foo\n\n<style>color: #f00</style> bar",
+		"<p>foo</p>\n\n<p>color: #f00 bar</p>\n",
+	}, TestParams{
+		HTMLFlags: SkipStyle,
+	})
+}
+
 func TestUseXHTML(t *testing.T) {
 	doTestsParam(t, []string{
 		"---",