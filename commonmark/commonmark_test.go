9cdc0af6bb44190d81697bd6d56a80063578c954
Cameron Moore <moorereason@gmail.com>
2018-08-31 16:10:45 -0500

    Refactor CommonMark tests to be Go tests
    This commit refactors the CommonMark tests as actual Go tests. The tests
    use Go sub-tests for each CommonMark section, so you can now select a
    subset of tests (using `-run=`).
    
    I tried to maintain the report features, but they are printed above the
    FAIL notices (a small incentive to fix some tests). The report now only
    shows sections that were tested if you selected a subset of tests.
    
    The tests are protected by a build tag now, so this branch could be
    merged upstream without breaking anything.
    
    The JSON file has also been updated to the latest from the CommonMark
    project.

--- commonmark/commonmark_test.go
+++ commonmark/commonmark_test.go
@@ -1,4 +1,15 @@
-package main
+//
+// Blackfriday Markdown Processor
+// Available at http://github.com/russross/blackfriday
+//
+// Copyright © 2018 Russ Ross <russ@russross.com>.
+// Distributed under the Simplified BSD License.
+// See README.md for details.
+//
+
+// +build commonmark
+
+package commonmark_test
 
 import (
 	"encoding/json"
@@ -6,8 +17,9 @@ import (
 	"io/ioutil"
 	"log"
 	"sort"
+	"testing"
 
-	blackfriday "github.com/russross/blackfriday.v2"
+	blackfriday "github.com/russross/blackfriday"
 )
 
 const testsDump = "testdata/commonmark-tests-dump.json"
@@ -52,12 +64,10 @@ func prepStats(tests []commonMarkTestCase) *stats {
 		if !ok {
 			s = &sectionStats{}
 		}
-		s.numTests++
 		statsPerSection[test.Section] = s
 	}
 	return &stats{
 		perSection: statsPerSection,
-		numTests:   len(tests),
 	}
 }
 
@@ -68,6 +78,10 @@ func reportStats(s *stats) {
 	var coveredNames []string
 	var uncoveredNames []string
 	for section, stats := range s.perSection {
+		if stats.numTests == 0 {
+			continue
+		}
+
 		if stats.numTests == stats.numPass {
 			coveredSections.perSection[section] = stats
 			coveredNames = append(coveredNames, section)
@@ -80,27 +94,35 @@ func reportStats(s *stats) {
 	fmt.Println("Completely covered sections:")
 	for _, section := range coveredNames {
 		s := coveredSections.perSection[section]
-		fmt.Printf("%q: %d/%d tests passed\n", section, s.numTests, s.numPass)
+		fmt.Printf("  %q: %d/%d tests passed\n", section, s.numTests, s.numPass)
 	}
 	sort.Strings(uncoveredNames)
 	fmt.Println("\nRemaining sections:")
 	for _, section := range uncoveredNames {
 		stats := s.perSection[section]
-		fmt.Printf("%q: %d/%d tests passed\n", section, stats.numPass, stats.numTests)
+		fmt.Printf("  %q: %d/%d tests passed (%0.f%%)\n", section, stats.numPass, stats.numTests, float64(stats.numPass)/float64(stats.numTests)*100)
 	}
-	fmt.Printf("\nTotal %d/%d tests passed\n", s.numPass, s.numTests)
+	fmt.Printf("\nTotal %d/%d tests passed (%.0f%%)\n\n", s.numPass, s.numTests, float64(s.numPass)/float64(s.numTests)*100)
 }
 
-func main() {
+func TestCommonMark(t *testing.T) {
 	tests := load(testsDump)
 	stats := prepStats(tests)
 	for _, test := range tests {
-		s := stats.perSection[test.Section]
-		html := string(blackfriday.Run([]byte(test.Markdown)))
-		if html == test.HTML {
-			stats.numPass++
-			s.numPass++
-		}
+		t.Run(fmt.Sprintf("%s %d", test.Section, test.Example), func(t *testing.T) {
+			sect := stats.perSection[test.Section]
+			sect.numTests++
+			stats.numTests++
+
+			html := string(blackfriday.Run([]byte(test.Markdown)))
+			if html == test.HTML {
+				stats.numPass++
+				sect.numPass++
+				return
+			}
+
+			t.Errorf("Input: %q\nExpect: %q\nActual: %q", test.Markdown, test.HTML, html)
+		})
 	}
 	reportStats(stats)
 }
